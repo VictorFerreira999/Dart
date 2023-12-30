e6d629e84409790eff3e9b7b670ca541c3854a74
Victor <victorferreira9990@gmail.com>
2023-12-30 10:26:06 -0300

    Map

---
+++ OOP/map.dart
@@ -0,0 +1,16 @@
+void main() {
+  Map<int, String> numeros = Map();
+
+  numeros[1] = 'Um';
+  numeros[2] = 'Dois';
+  numeros[3] = 'Três';
+  numeros[100] = 'Cem';
+
+  Map<String, String> estados = Map();
+  estados['SP'] = 'São Paulo';
+  estados['RJ'] = 'Rio de Janeiro';
+
+  print(estados['SP']);
+  print(estados.keys);
+  print(estados.values);
+}
